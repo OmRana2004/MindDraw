eaf488fa338c249cc8d18111506d4f240b9b490d
OmRana2004 <omrana8755800322@gmail.com>
2025-10-18 11:52:34 +0530

    default connection ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,19 +4,31 @@ import { JWT_SECRET } from '@repo/backend-common/config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function checkUser(token: string): string | null {
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (typeof decoded == "string") {
+    return null;
+  }
+
+  if (!decoded || !decoded.userId) {
+    return null;
+  }
+
+  return decoded.userId;
+}
+
 wss.on('connection', function connection(ws, request) {
   const url = request.url;
-
   if (!url) {
     return;
   }
   const queryParams = new URLSearchParams(url.split('?')[1]);
   const token = queryParams.get('token') || "";
-  const decoded = jwt.verify(token, JWT_SECRET);
+  const userId = checkUser(token);
 
-  if (!decoded) {
+  if (!userId) {
     ws.close();
-    return;
   }
 
   ws.on('message', function message(data) {
