45301dd8414344f89d1ffc023cc106617efd2b6c
OmRana2004 <omrana8755800322@gmail.com>
2025-10-26 23:41:20 +0530

    canvas 1 rectangel

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,20 @@
+"use client"
+
+import { initDraw } from "@/app/draw"
+import { useEffect, useRef } from "react"
+
 export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    
+    useEffect(() => {
+        
+        if (canvasRef.current) {
+            initDraw(canvasRef.current)
+        }
+
+    }, [canvasRef])
+
     return <div>
-        om
+        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
     </div>
 }
